d2e417a51884a1a6f5758d53de19adc6f9691c91
Mohith003 <dmohith@nmsu.edu>
2022-03-17 11:48:26 -0600

    P3 and P4

--- Circles/src/edu/nmsu/cs/circles/Circle2Test.java
+++ Circles/src/edu/nmsu/cs/circles/Circle2Test.java
@@ -1,34 +1,37 @@
 package edu.nmsu.cs.circles;
 
 /***
- * Example JUnit testing class for Circle1 (and Circle)
+ * Example JUnit testing class for Circle2 (and Circle)
  *
  * - must have your classpath set to include the JUnit jarfiles - to run the test do: java
- * org.junit.runner.JUnitCore Circle1Test - note that the commented out main is another way to run
+ * org.junit.runner.JUnitCore Circle2Test - note that the commented out main is another way to run
  * tests - note that normally you would not have print statements in a JUnit testing class; they are
  * here just so you see what is happening. You should not have them in your test cases.
  ***/
 
 import org.junit.*;
 
-public class Circle1Test {
+public class Circle2Test
+{
 	// Data you need for each test case
-	private Circle1 circle1;
+	private Circle2 circle2;
 
 	//
 	// Stuff you want to do before each test case
 	//
 	@Before
-	public void setup() {
+	public void setup()
+	{
 		System.out.println("\nTest starting...");
-		circle1 = new Circle1(1, 2, 3);
+		circle2 = new Circle2(1, 2, 3);
 	}
 
 	//
 	// Stuff you want to do after each test case
 	//
 	@After
-	public void teardown() {
+	public void teardown()
+	{
 		System.out.println("\nTest finished.");
 	}
 
@@ -36,31 +39,33 @@ public class Circle1Test {
 	// Test a simple positive move
 	//
 	@Test
-	public void simpleMove() {
+	public void simpleMove()
+	{
 		Point p;
 		System.out.println("Running test simpleMove.");
-		p = circle1.moveBy(1, 1);
-		Assert.assertTrue(p.x == 2 && p.y == 3);
+		p = circle2.moveBy(1, 1);
+		Assert.assertTrue(p.x == 3 && p.y == 2);
 	}
 
 	//
 	// Test a simple negative move
 	//
 	@Test
-	public void simpleMoveNeg() {
+	public void simpleMoveNeg()
+	{
 		Point p;
 		System.out.println("Running test simpleMoveNeg.");
-		p = circle1.moveBy(-1, -1);
-		Assert.assertTrue(p.x == 0 && p.y == 1);
+		p = circle2.moveBy(-1, -1);
+		Assert.assertTrue(p.x == 1 && p.y == 0);
 	}
 
-	//
+		//
 	// Test by increasing the scale
 	@Test
 	public void testIncreaseScale() {
 		System.out.println("Running test testIncreaseScale.");
-		double b = circle1.scale(0);
-		double r = circle1.scale(5.0);
+		double b = circle2.scale(0);
+		double r = circle2.scale(5.0);
 		Assert.assertTrue((b - r) == -5);
 	}
 
@@ -70,8 +75,8 @@ public class Circle1Test {
 	@Test
 	public void testDecreaseScale() {
 		System.out.println("Running test testDecreaseScale.");
-		double b = circle1.scale(0);
-		double r = circle1.scale(-0.5);
+		double b = circle2.scale(0);
+		double r = circle2.scale(-0.5);
 		Assert.assertTrue((b - r) == 0.5);
 	}
 
@@ -81,9 +86,9 @@ public class Circle1Test {
 	@Test
 	public void testIntersectFirstQuad() {
 		System.out.println("Running test testIntersectFirstQuad.");
-		Point p = circle1.moveToCenter();
-		Circle1 one = new Circle1(2, 2, 2);
-		Assert.assertTrue(circle1.intersects(one));
+		Point p = circle2.moveToCenter();
+		Circle2 one = new Circle2(2, 2, 2);
+		Assert.assertTrue(circle2.intersects(one));
 	}
 
 	//
@@ -92,9 +97,9 @@ public class Circle1Test {
 	@Test
 	public void testIntersectSecondQuad() {
 		System.out.println("Running test testIntersectSecondQuad.");
-		Point p = circle1.moveToCenter();
-		Circle1 two = new Circle1(-2, 2, 2);
-		Assert.assertTrue(circle1.intersects(two));
+		Point p = circle2.moveToCenter();
+		Circle2 two = new Circle2(-2, 2, 2);
+		Assert.assertTrue(circle2.intersects(two));
 	}
 
 	//
@@ -103,9 +108,9 @@ public class Circle1Test {
 	@Test
 	public void testIntersectThirdQuad() {
 		System.out.println("Running test testIntersectThirdQuad.");
-		Point p = circle1.moveToCenter();
-		Circle1 three = new Circle1(-2, -2, 2);
-		Assert.assertTrue(circle1.intersects(three));
+		Point p = circle2.moveToCenter();
+		Circle2 three = new Circle2(-2, -2, 2);
+		Assert.assertTrue(circle2.intersects(three));
 	}
 
 	//
@@ -114,9 +119,9 @@ public class Circle1Test {
 	@Test
 	public void testIntersectFourthQuad() {
 		System.out.println("Running test testIntersectFourthQuad.");
-		Point p = circle1.moveToCenter();
-		Circle1 four = new Circle1(2, -2, 2);
-		Assert.assertTrue(circle1.intersects(four));
+		Point p = circle2.moveToCenter();
+		Circle2 four = new Circle2(2, -2, 2);
+		Assert.assertTrue(circle2.intersects(four));
 	}
 
 	//
@@ -125,16 +130,14 @@ public class Circle1Test {
 	@Test
 	public void testNonIntersectingCircle() {
 		System.out.println("Running test testNonIntersectingCircle.");
-		Point p = circle1.moveToCenter();
-		Circle1 nonIntersector = new Circle1(7, 5, 2);
-		Assert.assertTrue(!circle1.intersects(nonIntersector));
+		Point p = circle2.moveToCenter();
+		Circle2 nonIntersector = new Circle2(7, 5, 2);
+		Assert.assertTrue(!circle2.intersects(nonIntersector));
 	}
 
 	/***
-	 * NOT USED public static void main(String args[]) { try {
-	 * org.junit.runner.JUnitCore.runClasses(
-	 * java.lang.Class.forName("Circle1Test")); } catch (Exception e) {
-	 * System.out.println("Exception:
+	 * NOT USED public static void main(String args[]) { try { org.junit.runner.JUnitCore.runClasses(
+	 * java.lang.Class.forName("Circle2Test")); } catch (Exception e) { System.out.println("Exception:
 	 * " + e); } }
 	 ***/
 
